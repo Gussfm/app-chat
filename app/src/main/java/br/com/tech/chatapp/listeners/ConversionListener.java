1b0a4a5a218bb1f6fa087868b8bca7173ed99dd1
Gustavo H <gustavo1995henrique.gh@gmail.com>
2023-07-17 18:54:51 -0300

    Finish app

---
+++ app/src/main/java/br/com/tech/chatapp/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package br.com.tech.chatapp.listeners;
+
+import br.com.tech.chatapp.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked (User user);
+}
---
+++ app/src/main/java/br/com/tech/chatapp/listeners/UserListener.java
@@ -0,0 +1,8 @@
+package br.com.tech.chatapp.listeners;
+
+
+import br.com.tech.chatapp.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
